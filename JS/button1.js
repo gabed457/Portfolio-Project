ab9a8ca79feea91b43e018c21fd74fbf8567a076
Gabriel King Driggs <gabe.driggs@gmail.com>
2015-09-03 08:34:16 -0600

    waef

---
+++ JS/button1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gabedriggs1 on 8/31/15.
+ */
+
+    function swapform() {
+    var visable
+    var hidden
+    visable = document.getElementById("visible").innerHTML;
+    hidden = document.getElementById("theform").innerHTML;
+    document.getElementById("visible").innerHTML = hidden;
+}
